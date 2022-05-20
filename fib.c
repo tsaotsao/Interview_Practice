7788327e657192d32e439737961d906a109a7fad
Jerry <zxcv45623@gmail.com>
2022-05-21 01:46:59 +0800

    add Matrix solution

--- fib.c
+++ fib.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
-
+#include "fib_MatMultiple.h"
 /**
- * @brief fibonacci sequence recursive method  O(n)
+ * @brief fibonacci sequence recursive method O(n)
  *        
  * @param n -- index of the sequence
  * @return int 
@@ -12,10 +12,10 @@ int fib_rec(int n)
 {
     if(n==0 | n==1) return n;
     else return (fib_rec(n-1)+fib_rec(n-2));
-}
 
+}
 /**
- * @brief dynamic programming method:
+ * @brief dynamic programming method
  *        use MEMO to record the sequence 
  * 
  * @param n 
@@ -35,11 +35,39 @@ uint64_t fib_DP(uint16_t n)
     return memo[n]; // return with uint64_t, test for big number
 }
 
+/**
+ * @brief Space Optimized method 
+ *           -- which means it saves space
+ * 
+ * @param n 
+ * @return int 
+ */
+int fib_SpaceOptimized(int n)
+{
+    int a = 0, b = 1, c, i;
+    if(n==0)
+        return a;
+    for(i=2; i<=n; i++)
+    {
+        c = a+b;
+        a = b;
+        b = c; 
+    }
+    return b;
+}
+
+int fib_MatrixSol(int n)
+{
+    if(n == 0 | n == 1) return n;
+    int fibMatrix[2][2] = {{1, 1}, {1, 0}};
+    power(fibMatrix, n);
+    return fibMatrix[0][0];
+}
 
 
 int main(int argc, char **argv)
 {
-    uint64_t ans = fib_DP(atoi(argv[1]));
+    uint64_t ans = fib_MatrixSol(atoi(argv[1]));
     printf("%llu\n", ans);
     return 0;
 }
---
+++ fib_MatMultiple.h
@@ -0,0 +1,6 @@
+#ifndef _power
+#define _power
+#endif
+void MatMultiple(int F[2][2], int M[2][2]);
+void power(int F[2][2], int n);
+
